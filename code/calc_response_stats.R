ec00ebf25e6e3087166ae4b6cc8d23456e554650
William L. Close <close.will@gmail.com>
2018-09-06 19:43:17 -0400

    created function to calc response freq for list of stratified dfs
    Allows input of desired questions and the list of stratified data frames created by stratify_data.R

--- code/calc_response_stats.R
+++ code/calc_response_stats.R
@@ -21,6 +21,8 @@ multi_choice_question_list <- setdiff(question_list, typed_question_list)
 
 # multiple choice statistics ----------------------------------------------
 
+########## generating responses for entire dataset ##########
+
 # function for generating individual response frequencies
 get_response_freq <- function(question_no_chr) { # question_no_chr = question number in character format (aka need quotes)
   freq <- tidy_survey_data %>% # assigning the output to a variable
@@ -31,47 +33,63 @@ get_response_freq <- function(question_no_chr) { # question_no_chr = question nu
   return(freq) # returns the calculated/modified data frame
 }
 
-# # test to make sure function works
-# get_response_freq("Q37")
-
 # use map() to generate a named list containing response freq data frames for muliple choice questions
 response_freq <- map(multi_choice_question_list, get_response_freq) %>% # running get_response_freq for each item in question list
   set_names(multi_choice_question_list) # naming the items in the list by question number
 
-# # testing indexing ability of map() output
-# response_freq[[1]]
-# response_freq$Q12
 
 
-####### need to figure out how to setup functions to allow input of lists of dataframes and questions
+########## END ##########
 
-# inputs needed for function
-# 1 data = strat list (list of dataframes with response_ids classified accodring to strat categories)
-# 2 question list = list of multiple choice questions to be used for generating response frequencies and graphs
+########## generating responses based on stratifications ##########
 
-# function for generating individual response frequencies
-test_get_response_freq <- function(question_no_chr) { # question_no_chr = question number in character format (aka need quotes)
-  freq <- tidy_survey_data %>% # assigning the output to a variable
+# altering the function from before to incorporate group_by strat_id which is assigned from stratify_data.R
+get_strat_response_freq <- function(df, question_no_chr) { # question_no_chr = question number in character format (aka need quotes)
+  freq <- df %>% # assigning the output to a variable
     filter(question_no == question_no_chr) %>% # selects rows containing responses for specific questions
-    group_by(question_no, subquestion_no, question, response) %>% # groups by question and response to provide summary stats
+    group_by(strat_id, question_no, subquestion_no, question, response) %>% # groups by question and response to provide summary stats
     summarize(n = n()) %>% # creates col for number of a given response (n)
     mutate(percent_freq = n/sum(n)*100) # creates col for percent of total responses attributed to a given response
   return(freq) # returns the calculated/modified data frame
 }
 
-get_response_data <- function(x) {
-  data <- map_df(x, get_response_freq)
+# creating a function formatted for use with map
+aggregate_strat_response_freq <- function(list_of_strat_df) {
+  # creating a dataframe for use with pmap
+  arguments <- data_frame(df = list(list_of_strat_df), # 1st col = input df repeated to be same length as number of questions
+                   question_no_chr = c(multi_choice_question_list)) # 2nd col = question names for use in filtering
+  data <- pmap_df(arguments, get_strat_response_freq) # collating data during map and assigning output
   return(data)
 }
 
-response_freq <- map(multi_choice_question_list, get_response_data)
+# checking the function with the strat_id classified data
+strat_response_freq <- map(strat_data, aggregate_strat_response_freq)
 
+
+
+########## END ########## 
 
 
 
 # typed statistics --------------------------------------------------------
 
+
+
+
+
+
+
 # use this section to generate tables for plotting written responses
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -90,3 +108,30 @@ response_freq <- map(multi_choice_question_list, get_response_data)
 # 
 #   summarize(n = n()) %>% # creates col for number of a given response (n)
 #   mutate(percent_freq = n/sum(n)*100) 
+
+# # test to make sure function works when run on entire dataset
+# get_response_freq("Q37")
+
+# # testing indexing ability of map() output
+# response_freq[[1]]
+# response_freq$Q12
+
+# # example of flow for generating stratified response freqs and graphing (df and question_no would be fed in to function)
+# strat_data$satisfaction %>% 
+#   filter(question_no == "Q12") %>% 
+#   group_by(strat_id, response) %>% 
+#   summarize(n = n()) %>% 
+#   mutate(percent_freq = n/sum(n)*100) %>% 
+#   ggplot(aes(x = strat_id, y = percent_freq, fill = response)) +
+#   geom_bar(stat = "identity")
+
+# checking the output of the function (need to make sure group_by is in correct order to generated desired response freqs)
+# check <- get_strat_response_freq(strat_data$satisfaction, "Q12")
+
+# checking the dataframe via graphing
+# check %>%
+#   ggplot(aes(x = strat_id, y = percent_freq, fill = response)) +
+#   geom_bar(stat = "identity")
+
+# # doesn't work on original dataset because there isn't a strat_id col (need to use original form of get_response_freq for regular dataset)
+# aggregate_strat_response_freq(tidy_survey_data)
