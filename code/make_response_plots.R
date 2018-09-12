96edff41c253280c4a2071f57816798676b0aa33
William L. Close <close.will@gmail.com>
2018-09-12 01:57:45 -0400

    Rewrote/wrote plotting functions
    now accepts a list of questions as a variable then iterates through each question for each df in the list of stratified dfs giving a list of lists of lists that can be quickly indexed to find individual graphs of interest

--- code/make_response_plots.R
+++ code/make_response_plots.R
@@ -20,52 +20,107 @@ text_wrapper <- function(x, ...)
 
 # NOTE: Q22 responses need to be broken up and retabulated before graphing
 
-# making function to generate plots for multi choice questions
-make_response_plot <- function(df) { # df = properly formatted data frame with data
-  freq_plot <- df %>% # assigning the output
-    ggplot() +
-    geom_bar(aes(x = response, y = percent_freq, fill = response, group = subquestion_no), # adding bars
-             color = "black", show.legend = F, stat = "identity", width = 0.75) +
-    geom_text(aes(x = response, y = percent_freq, label = paste0(round(percent_freq, 1), "%")), vjust = -0.5) + # adding response freq over bars
-    scale_y_continuous(limits = c(0,100), expand = c(0,0)) + # formatting y axis
-    scale_x_discrete(labels = c(str_replace_all(df$response, "_", " "))) + # reformatting axis labels to look nice
-    labs(title = text_wrapper(str_replace_all(unique(df$question), "_", " "), width = 60), # reformatting remainging labels to look nice
-         y = "Proportion of Postdoctoral Respondents (%)",
-         x = "Responses") +
-    theme_classic() + # changing color scheme, etc.
-    theme(plot.title = element_text(hjust = 0.5)) # centering the title over the plot
-  return(freq_plot) # returning the plot to environment
+# creating a plotting function that makes new graph for each question
+make_response_plot <- function(df, question_no_chr) {
+  df %>% 
+    filter(question_no == question_no_chr & !is.na(response) & response != "Prefer_not_to_answer") %>% # removing ambiguous answers from plots
+    ggplot(aes(x = strat_id, y = percent_freq, fill = response)) + # plotting the stratified categories by response
+    geom_bar(stat = "identity") + # bar plot
+    facet_wrap(~ question) # makes each question or group of subquestions their own plots/group of plots
 }
 
-# testing response plotting function
-make_response_plot(response_freq$Q6)
+# # testing out the plotting function
+# make_response_plot(strat_response_freq$language, "Q35")
 
-# using map() to generate plots for each data frame contained within response_freq list
-response_plots <- map(response_freq, make_response_plot) # don't need to specify names b/c preserves names from response_freq
+# making function to cycle through each question for each data frame
+make_all_response_plots <- function(strat_response_freq_df, question_no_chr_list) {
+  arguments <- data_frame(df = list(strat_response_freq_df), # 1st col = input df repeated to be same length as number of questions
+                          question_no_chr = c(question_no_chr_list))
+  data <- pmap(arguments, make_response_plot) %>% # collating data during map and assigning output
+    set_names(question_no_chr_list)
+  return(data)
+}
 
-# verifying indexing ability and plot appearance
-response_plots$Q12
+# # testing the pmap function
+# test_output <- make_all_response_plots(strat_response_freq$language, multi_choice_question_list)
+# test_output[[1]]
+# test_output$Q15
 
 
+# making plots for all of the stratified data sets
+# NOTE: question_no_chr_list is passed on to make_all_response_plots()
+response_plots <- map(.x = strat_response_freq, .f = make_all_response_plots, question_no_chr_list = multi_choice_question_list)
+
+# veriftying the results
+response_plots$college_school[[1]]
+response_plots$college_school$Q35
+
+
+
+# typed plots -------------------------------------------------------------
+
+
+
+
+
+
+# look into ideas to plot responses to typed questions
+
+
+
+
+
+
+
+
+# notes -------------------------------------------------------------------
 
 # making function to generate plots for multi choice questions
-make_stack_response_plot <- function(df) { # df = properly formatted data frame with data
-  freq_plot <- df %>% # assigning the output
-    ggplot() +
-    geom_bar(aes(x = question_no, y = percent_freq, fill = response), # adding bars
-             color = "black", show.legend = T, stat = "identity", width = 0.75, position = "stack") +
-    #geom_text(aes(x = response, y = percent_freq, label = paste0(round(percent_freq, 1), "%")), vjust = -0.5) + # adding response freq over bars
-    scale_y_continuous(limits = c(0,100), expand = c(0,0)) + # formatting y axis
-    #scale_x_discrete(labels = c(str_replace_all(df$question, "_", " "))) + # reformatting axis labels to look nice
-    labs(#title = text_wrapper(str_replace_all(unique(df$question), "_", " "), width = 60), # reformatting remainging labels to look nice
-         y = "Proportion of Postdoctoral Respondents (%)",
-         x = "Responses") +
-    #coord_flip() +
-    theme_classic() + # changing color scheme, etc.
-    theme(plot.title = element_text(hjust = 0.5)) + # centering the title over the plot
-    facet_wrap( ~ question)
-  return(freq_plot) # returning the plot to environment
-}
+# make_response_plot <- function(df) { # df = properly formatted data frame with data
+#   freq_plot <- df %>% # assigning the output
+#     ggplot() +
+#     geom_bar(aes(x = response, y = percent_freq, fill = response, group = subquestion_no), # adding bars
+#              color = "black", show.legend = F, stat = "identity", width = 0.75) +
+#     geom_text(aes(x = response, y = percent_freq, label = paste0(round(percent_freq, 1), "%")), vjust = -0.5) + # adding response freq over bars
+#     scale_y_continuous(limits = c(0,100), expand = c(0,0)) + # formatting y axis
+#     scale_x_discrete(labels = c(str_replace_all(df$response, "_", " "))) + # reformatting axis labels to look nice
+#     labs(title = text_wrapper(str_replace_all(unique(df$question), "_", " "), width = 60), # reformatting remainging labels to look nice
+#          y = "Proportion of Postdoctoral Respondents (%)",
+#          x = "Responses") +
+#     theme_classic() + # changing color scheme, etc.
+#     theme(plot.title = element_text(hjust = 0.5)) # centering the title over the plot
+#   return(freq_plot) # returning the plot to environment
+# }
+# 
+# # testing response plotting function
+# make_response_plot(response_freq$Q6)
+# 
+# # using map() to generate plots for each data frame contained within response_freq list
+# response_plots <- map(response_freq, make_response_plot) # don't need to specify names b/c preserves names from response_freq
+# 
+# # verifying indexing ability and plot appearance
+# response_plots$Q12
+
+
+
+# # making function to generate plots for multi choice questions
+# make_stack_response_plot <- function(df) { # df = properly formatted data frame with data
+#   freq_plot <- df %>% # assigning the output
+#     ggplot() +
+#     geom_bar(aes(x = question_no, y = percent_freq, fill = response), # adding bars
+#              color = "black", show.legend = T, stat = "identity", width = 0.75, position = "stack") +
+#     #geom_text(aes(x = response, y = percent_freq, label = paste0(round(percent_freq, 1), "%")), vjust = -0.5) + # adding response freq over bars
+#     scale_y_continuous(limits = c(0,100), expand = c(0,0)) + # formatting y axis
+#     #scale_x_discrete(labels = c(str_replace_all(df$question, "_", " "))) + # reformatting axis labels to look nice
+#     labs(#title = text_wrapper(str_replace_all(unique(df$question), "_", " "), width = 60), # reformatting remainging labels to look nice
+#          y = "Proportion of Postdoctoral Respondents (%)",
+#          x = "Responses") +
+#     #coord_flip() +
+#     theme_classic() + # changing color scheme, etc.
+#     theme(plot.title = element_text(hjust = 0.5)) + # centering the title over the plot
+#     facet_wrap( ~ question)
+#   return(freq_plot) # returning the plot to environment
+# }
 
 # make_stack_response_plot(response_freq$Q35)
 # 
@@ -81,43 +136,4 @@ make_stack_response_plot <- function(df) { # df = properly formatted data frame 
 #   ggplot(aes(x = strat_id, y = percent_freq, fill = response)) +
 #   geom_bar(stat = "identity") +
 #   facet_wrap(~ subquestion_no)
-
-# creating a plotting function that makes new graph for each question
-test_plot_function <- function(df, question_no_chr) {
-  df %>% 
-    filter(question_no == question_no_chr & !is.na(response) & response != "Prefer_not_to_answer") %>% 
-    ggplot(aes(x = strat_id, y = percent_freq, fill = response)) +
-    geom_bar(stat = "identity") +
-    facet_wrap(~ subquestion_no)
-}
-
-# testing it out
-test_plot_function(strat_response_freq$language, "Q35")
-
-# making function to allow use of pmap to iterate through each plot needing to be made
-# NOTE: can make list of input questions as a new variable to be populated when ran (makes it more DRY)
-test_function <- function(list_strat_response_freq_df) {
-  arguments <- data_frame(df = list(list_strat_response_freq_df), # 1st col = input df repeated to be same length as number of questions
-                          question_no_chr = c(multi_choice_question_list))
-  data <- pmap(arguments, test_plot_function) # collating data during map and assigning output
-  return(data)
-}
-
-
-# testing the pmap function
-test_output <- test_function(strat_response_freq$language, multi_choice_question_list)
-test_output
-
-# # making plots for all of the stratified data sets
-# map(strat_response_freq, test_function)
-
-
-# typed plots -------------------------------------------------------------
-
-# look into ideas to plot responses to typed questions
-
-
-
-# notes -------------------------------------------------------------------
-
 
