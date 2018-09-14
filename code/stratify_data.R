36b1fcd9f9874db2330c003dee1f9de295496b7b
William L. Close <close.will@gmail.com>
2018-09-14 15:05:31 -0400

    updated labels for strat_id to make plotting easier

--- code/stratify_data.R
+++ code/stratify_data.R
@@ -23,34 +23,34 @@ source("code/tidy_survey.R")
 # 9 Satisfied v. not satisfied
 
 classified_survey_data <- tidy_survey_data %>% 
-  mutate(stratifications = case_when(question_no == "Q6" & response == "Medicine" ~ "umms", # school of medicine
-                                     question_no == "Q6" & response == "Literature,Science,and_the_Arts" ~ "lsa", # lit, science, and arts
-                                     question_no == "Q6" & response == "Engineering" ~ "eng", # engineering
-                                     question_no == "Q6" & !is.na(response) & response != "Prefer_not_to_answer" ~ "other_school", # classifying other schools as "Other"
-                                     question_no == "Q10" & !is.na(response) ~ "domestic", # anything other than NA = citizen
-                                     question_no == "Q11" & !is.na(response) ~ "international", # anything other NA = non-citizen
-                                     question_no == "Q12" & response == "Yes" ~ "english_first",
-                                     question_no == "Q12" & !is.na(response) & response != "Prefer_not_to_answer" ~ "english_acquired",
-                                     question_no == "Q17" & response == "Yes" ~ "dependents",
-                                     question_no == "Q17" & response == "No" ~ "no_dependents",
-                                     question_no == "Q16" & response == "Single,never_married" ~ "single_unmarried",
-                                     question_no == "Q16" & response == "Married_or_in_domestic_partnership" ~ "married_partnership",
-                                     question_no == "Q16" & response == "Widowed" ~ "widowed",
-                                     question_no == "Q16" & response == "Divorced_or_separated" ~ "divorced_separated",
-                                     question_no == "Q14" & response == "Female" ~ "female",
-                                     question_no == "Q14" & response == "Male" ~ "male",
-                                     question_no == "Q14" & response == "Non-binary" ~ "non_binary",
-                                     question_no == "Q15" & response == "Yes" ~ "under_represented",
-                                     question_no == "Q15" & response == "No" ~ "well_represented",
-                                     question_no == "Q31" & response == "Academic_–_research" ~ "academic_research",
-                                     question_no == "Q31" & response == "Academic_–_teaching" ~ "academic_teaching",
-                                     question_no == "Q31" & response == "Non-academic" ~ "non_academic",
-                                     question_no == "Q31" & response == "Unsure" ~ "career_unsure",
-                                     question_no == "Q8" & response == "Yes" ~ "first_postdoc",
-                                     question_no == "Q8" & response == "No" ~ "not_first_postdoc",
-                                     question_no == "Q39" & response == "Agree" ~ "satisfied",
-                                     question_no == "Q39" & response == "Disagree" ~ "unsatisfied",
-                                     question_no == "Q39" & response == "Neutral" ~ "neutral_satisfaction",
+  mutate(stratifications = case_when(question_no == "Q6" & response == "Medicine" ~ "UMMS", # school of medicine
+                                     question_no == "Q6" & response == "Literature,Science,and_the_Arts" ~ "LSA", # lit, science, and arts
+                                     question_no == "Q6" & response == "Engineering" ~ "ENG", # engineering
+                                     question_no == "Q6" & !is.na(response) & response != "Prefer_not_to_answer" ~ "Other", # classifying other schools as "Other"
+                                     question_no == "Q10" & !is.na(response) ~ "Domestic", # anything other than NA = citizen
+                                     question_no == "Q11" & !is.na(response) ~ "International", # anything other NA = non-citizen
+                                     question_no == "Q12" & response == "Yes" ~ "EFL",
+                                     question_no == "Q12" & !is.na(response) & response != "Prefer_not_to_answer" ~ "EAL",
+                                     question_no == "Q17" & response == "Yes" ~ "Dependents",
+                                     question_no == "Q17" & response == "No" ~ "No_dependents",
+                                     question_no == "Q16" & response == "Single,never_married" ~ "Single_(unmarried)",
+                                     question_no == "Q16" & response == "Married_or_in_domestic_partnership" ~ "Married/domestic_partnership",
+                                     question_no == "Q16" & response == "Widowed" ~ "Widowed",
+                                     question_no == "Q16" & response == "Divorced_or_separated" ~ "Divorced/separated",
+                                     question_no == "Q14" & response == "Female" ~ "Female",
+                                     question_no == "Q14" & response == "Male" ~ "Male",
+                                     question_no == "Q14" & response == "Non-binary" ~ "Non-binary",
+                                     question_no == "Q15" & response == "Yes" ~ "Under-represented",
+                                     question_no == "Q15" & response == "No" ~ "Well-represented",
+                                     question_no == "Q31" & response == "Academic_–_research" ~ "Academic_(research)",
+                                     question_no == "Q31" & response == "Academic_–_teaching" ~ "Academic_(teaching)",
+                                     question_no == "Q31" & response == "Non-academic" ~ "Non-academic",
+                                     question_no == "Q31" & response == "Unsure" ~ "Unsure",
+                                     question_no == "Q8" & response == "Yes" ~ "First_postdoc",
+                                     question_no == "Q8" & response == "No" ~ ">1_postdoc",
+                                     question_no == "Q39" & response == "Agree" ~ "Satisfied",
+                                     question_no == "Q39" & response == "Disagree" ~ "Unsatisfied",
+                                     question_no == "Q39" & response == "Neutral" ~ "Neutral",
                                      TRUE ~ NA_character_))
 
 
@@ -74,16 +74,16 @@ list_names <- c("college_school", "postdoc_no", "residency", "language", "gender
 
 # creating a named list of all of the desired breakdowns for comparisons then naming the output list
 # the output will maintain the names given to the items in the input list which is why naming at this step is so important
-strat_list <- list(c("umms", "eng", "lsa", "other"), 
-                   c("first_postdoc", "not_first_postdoc"),
-                   c("domestic", "international"),
-                   c("english_first", "english_acquired"),
-                   c("male", "female", "non_binary"),
-                   c("under_represented", "well_represented"),
-                   c("single_unmarried", "married_partnership", "widowed", "divorced_separated"),
-                   c("dependents", "no_dependents"),
-                   c("academic_research", "academic_teaching", "non_academic", "career_unsure"),
-                   c("satisfied", "unsatisfied", "neutral_satisfaction")) %>% 
+strat_list <- list(c("UMMS", "ENG", "LSA", "Other"), 
+                   c("First_postdoc", ">1_postdoc"),
+                   c("Domestic", "International"),
+                   c("EFL", "EAL"),
+                   c("Male", "Female", "Non-binary"),
+                   c("Under-represented", "Well-represented"),
+                   c("Single_(unmarried)", "Married/domestic_partnership", "Widowed", "Divorced/separated"),
+                   c("Dependents", "No_dependents"),
+                   c("Academic_(research)", "Academic_(teaching)", "Non-academic", "Unsure"),
+                   c("Satisfied", "Unsatisfied", "Neutral")) %>% 
   set_names(list_names)
 
 # setting up a function to allow use of map to iterate through each set of stratification classifications
