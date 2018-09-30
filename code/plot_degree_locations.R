28ec6e8018ea6e89941d0f4d0567383f6a8a5625
William L. Close <close.will@gmail.com>
2018-09-30 01:23:01 -0400

    cleaning up and consolidating code

--- code/plot_degree_locations.R
+++ code/plot_degree_locations.R
@@ -17,75 +17,245 @@ us_states_territories <- us_states_territories %>%
 
 
 
-# maps of degree regions ------------------------------------------------
+# plotting us degree locations --------------------------------------------
+
+# packages needed to plot map
+library(mapproj) # needed to maintain dimensions
+library(fiftystater) # needed to plot HI and AK in insets
+library(viridis) # needed for color scaling
 
 # NOTE: need to adjust question to apply to Ph.D.s, M.D.s, and J.D.s (do J.D.s count as postdocs?)
 # NOTE: some US citizens completed degrees outside the US, need to adjust question if/else flow for citizenship question
 # NOTE: need to give a list of states/countries to select from
-
-
-
-########## generating map of regions within the US ##########
-
-# creating function to extract the us state from the responses and convert all abbreviations to full names for uniformity and plotting
-# added in step to filter data to only relevant question about regions within us
-extract_us_region <- function(df) {
-  us_region <- c(us_states_territories$abb, us_states_territories$name) # creating vector of states and abbreviations
-  us_abb_to_region <- us_states_territories$name %>% # creates a named vector to enable extraction based on full name
-    set_names(us_states_territories$abb)
-  data <- df %>%
-    filter(question_no == "Q10" & !is.na(response)) %>% 
-    mutate(response = str_replace_all(response, "_", " "),
-           region = tolower(case_when(str_detect(response, regex(paste(paste0("\\b", us_region, "\\b"), collapse = "|"), ignore_case = T)) ~ # detecting anything in any case that matches string with word boundaries on both ends
-                                          str_extract(response, regex(paste(paste0("\\b", us_region, "\\b"), collapse = "|"), ignore_case = T)),  # extracting the state information to a new col
-                                        TRUE ~ NA_character_)), 
-           region = ifelse(region %in% names(us_abb_to_region), us_abb_to_region[region], region)) # converts region to state abbreviation based on presence of state name
-  return(data)
-}
-
-# extracting us region data and adding as a new col creating state_data df
-us_data <- extract_us_region(tidy_survey_data)
-
-# # getting counts for each state/territory
-# us_data %>% 
-#   group_by(question_no, subquestion_no, question, region) %>% # groups by question and response to provide summary stats
-#   summarize(n = n()) # creates col for number of a given response (n)
+# NOTE: only plots us states, still need to work on including us territories
+# NOTE: need to move alaska and hawaii to be on plot and draw box around
 
 
 
 # making function to generate counts of regions
 get_region_counts <- function(region_df, parent_df=tidy_survey_data) {
+  
   # requires parent df
   region_number <- parent_df %>% 
     filter(question_no == "Q10" & !is.na(response) | question_no == "Q11" & !is.na(response)) %>% 
     select(response_id) %>%
     unique(.) %>%
     nrow()
+  
   # requires us only df
   data <- region_df %>% 
     group_by(question_no, subquestion_no, question, region) %>% # groups by question and response to provide summary stats
     summarize(n = n()) %>% # creates col for number of a given response (n)
     mutate(percent_freq = n/region_number*100) %>% 
     ungroup()
+  
   return(data)
 }
 
-get_region_counts(us_data)
+
+
+# creating function to extract the us state from the responses and convert all abbreviations to full names for uniformity and plotting
+calc_us_degree_freq <- function(df) {
+  
+  # creating vector of states and abbreviations
+  us_region <- c(us_states_territories$abb, us_states_territories$name) 
+  
+  # creates a named vector to enable extraction based on full name
+  us_abb_to_region <- us_states_territories$name %>% 
+    set_names(us_states_territories$abb)
+  
+  # creating df of us regions (states)
+  regions <- df %>%
+    filter(question_no == "Q10" & !is.na(response)) %>% # filters out non-us data
+    mutate(response = str_replace_all(response, "_", " "),
+           region = tolower(case_when(str_detect(response, regex(paste(paste0("\\b", us_region, "\\b"), collapse = "|"), ignore_case = T)) ~ # detecting anything in any case that matches string with word boundaries on both ends
+                                          str_extract(response, regex(paste(paste0("\\b", us_region, "\\b"), collapse = "|"), ignore_case = T)),  # extracting the state information to a new col
+                                        TRUE ~ NA_character_)), 
+           region = ifelse(region %in% names(us_abb_to_region), us_abb_to_region[region], region)) # converts region to state abbreviation based on presence of state name
+  
+  region_freqs <- get_region_counts(regions) %>% 
+    select(region, percent_freq) %>% # selects only the region and count cols
+    right_join(select(us_states_territories, name), by = c("region" = "name")) # joining with list of states/territories to add any missing
+  
+  return(region_freqs)
+}
 
 
 
+# extracting us region data and adding as a new col creating state_data df
+us_degree_freq <- calc_us_degree_freq(tidy_survey_data)
 
+test2 <- map_data("state") %>% 
+  full_join(test)
+
+test2 %>% 
+  filter(region == "alaska")
+
+test <- fifty_states %>% 
+  filter(id == "alaska" | id == "hawaii") %>% 
+  select(long, lat, order, region = id)
+
+str(fifty_states$group)
+
+# making function for mapping locations within the us
+plot_us_degree_freq <- function(df) {
+  
+  # creating df of all us regions (state) and plotting coordinates
+  states_map <- map_data("state")
+  
+  inset_coord <- tibble(x = c(-123.9834,-112.3072,-112.3072,-123.9834,-123.9834,-112.2745,-102.9840,-102.9840,-112.2745,-112.2745),
+                        y = c(23.01948,23.01948,31.24104,31.24104,23.01948,23.01704,23.01704,28.60362,28.60362,23.01704),
+                        id = rep(c("alaska", "hawaii"), each = 5))
+  
+  # plotting function for us state data
+  plot <- df %>% 
+    ggplot(aes(map_id = region)) +
+    geom_map(aes(fill = percent_freq), map = test2, color = "black", size = 0.2) + # plots the map
+    expand_limits(x = test2$long, y = test2$lat) +
+    annotate(geom = "path", x = inset_coord$x, y = inset_coord$y, group = inset_coord$id) +
+    scale_fill_continuous(type = "viridis", na.value = "white") + # making NA values = white and scaling using viridis palette
+    labs(title = "Locations of U.S. Ph.D. Granting Institutions for University of Michigan Postdocs",
+         fill = "Percent of Respondents") +
+    coord_map() + # gives map proper dimensions
+    #fifty_states_inset_boxes() + # package function to add boxes around insets for AK and HI
+    theme(axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank(),
+          panel.background = element_rect(fill = "white"))
+  
+  # returning plotted us map
+  return(plot)
+}
+
+
+# plotting the data
+#us_degree_regions <- 
+plot_us_degree_freq(us_degree_freq)
+
+
+# saving us degree freq map
+ggsave(filename = "results/us_degree_locations.png", plot = us_degree_regions, width = 20, dpi = 300)
+
+
+
+# Things to work on:
+# Include map of world around it
+# Include us territories
+# Change percent of respondents to be based on total dataset
+
+
+# plotting world degree locations -----------------------------------------
+
+library(ggalt) # needed for coord_proj
+
+# creating function to extract country names from responses
+calc_world_degree_freq <- function(df) {
+  
+  # creating a list of all countries with data available for plotting
+  countries <- map_data("world") %>% # making list of countries with available mapping information
+    pull(region) %>% # pulls out list of countries
+    unique(.) # makes list of unique countries
+  
+  # extracting region information from responses
+  regions <- df %>%
+    filter(question_no == "Q11" & !is.na(response)) %>% # filters down to country info
+    mutate(response = str_replace_all(response, "_", " "), # replaces all "_" with spaces to allow text parsing
+           region = tolower(case_when(str_detect(response, regex(paste(paste0("\\b", countries, "\\b"), collapse = "|"), ignore_case = T)) ~ # detecting anything in any case that matches country names with word boundaries on both ends
+                                          str_extract(response, regex(paste(paste0("\\b", countries, "\\b"), collapse = "|"), ignore_case = T)),  # extracting the country information to a new col
+                                        TRUE ~ NA_character_))) # any country not in the list of available countries is labelled with NA
+  
+  # calculating the frequency of responses for individual regions
+  region_freqs <- get_region_counts(regions) %>% 
+    select(region, percent_freq) %>% # selects only the region and count cols 
+    right_join(select(as_data_frame(tolower(countries)), value), by = c("region" = "value")) # joining with list of countries to add missing ones
+  
+  # returning freq df
+  return(region_freqs)
+}
+
+
+
+# creating df of data for plotting
+world_degree_freq <- calc_world_degree_freq(tidy_survey_data)
+
+
+
+# creating function for plotting world map information
+plot_world_degree_freq <- function(df) {
+  
+  # creating df of plotting coordinates for world map
+  world_map <- map_data("world") %>% # getting data
+    mutate(region = tolower(region)) # transforming all country names to lower case for matching with survey data
+  
+  # main plotting function to make world map
+  plot <- df %>% 
+    ggplot(aes(map_id = region)) + # map_id is required aes for geom_map
+    geom_map(aes(fill = percent_freq), map = world_map, colour = "black", size = 0.25) + # plots the world map
+    expand_limits(x = c(-179,179), y = c(-75,89)) + # expands plot limits to encompass data, x = longitude, y = latitude
+    scale_fill_continuous(type = "viridis", na.value = "white") + # making NA values = white and scaling using viridis palette
+    labs(title = "Locations of Ph.D. Granting Institutions for University of Michigan Postdocs", # setting chart labels
+         fill = "Respondents (%)") +
+    coord_proj(proj = "+proj=wintri") + # correcting for Earth curvature using Winkel tripel projection
+    theme(axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank(),
+          panel.background = element_rect(fill = "white"))
+  
+  # returning the map
+  return(plot)
+}
+
+# generating world map with postdoc degree freqs
+world_degree_map <- plot_world_degree_freq(world_degree_freq)
+
+world_degree_map
+
+# detaching conflicting packages ------------------------------------------
+
+detach("package:mapproj", unload = TRUE)
+detach("package:maps", unload = TRUE)
+
+
+# notes -------------------------------------------------------------------
+
+# # list of countries available for mapping
+# countries <- map_data("world") %>% # making list of countries with available mapping information
+#   pull(region) %>% # pulls out list of countries
+#   unique(.) # makes list of unique countries
+# 
+# # calculating freqs of countries
+# int_region_counts <- get_region_counts(int_data) %>% 
+#   select(region, percent_freq) %>% # selects only the region and count cols 
+#   right_join(select(as_data_frame(tolower(countries)), value), by = c("region" = "value")) # joining with list of countries to add missing ones
+
+
+
+# world_map <- map_data("world") %>% 
+#   mutate(region = tolower(region)) 
+# 
+# int_region_counts %>% 
+#   ggplot(aes(map_id = region)) +
+#   geom_map(map=world_map, aes(fill=percent_freq), colour="black", size=0.5) +
+#   expand_limits(x = world_map$long, y = world_map$lat) +
+#   scale_fill_continuous(type = "viridis", na.value = "white") +
+#   coord_proj("+proj=wintri")
+
+
+# # getting counts for each state/territory
+# us_data %>% 
+#   group_by(question_no, subquestion_no, question, region) %>% # groups by question and response to provide summary stats
+#   summarize(n = n()) # creates col for number of a given response (n)
 
 # # testing get_region_counts
 # get_location_counts(us_data)
 
-
 ##### turn this into a function ####
 # saving count data for us states and filling in missing states
-us_region_counts <- get_region_counts(us_data) %>% 
-  select(region, percent_freq) %>% # selects only the region and count cols
-  right_join(select(us_states_territories, name), by = c("region" = "name")) # joining with list of states/territories to add any missing
-
+# us_region_counts <- get_region_counts(us_data) %>% 
+#   select(region, percent_freq) %>% # selects only the region and count cols
+#   right_join(select(us_states_territories, name), by = c("region" = "name")) # joining with list of states/territories to add any missing
 
 # # working on plotting location information
 # crimes <- data.frame(state = tolower(rownames(USArrests)), USArrests)
@@ -103,41 +273,6 @@ us_region_counts <- get_region_counts(us_data) %>%
 #     facet_wrap( ~ variable)
 # }
 
-# packages needed to plot map
-library(mapproj) # needed to maintain dimensions
-library(fiftystater) # needed to plot HI and AK in insets
-library(viridis) # needed for color scaling
-
-# NOTE: only plots us states, still need to work on including us territories
-# NOTE: need to move alaska and hawaii to be on plot and draw box around
-
-# making function for mapping locations within the us
-plot_us_degree_regions <- function(df) {
-  states_map <- map_data("state")
-  plot <- df %>% 
-    ggplot(aes(map_id = region)) +
-    geom_map(aes(fill = percent_freq), map = states_map, color = "black", size = 0.2) + # plots the map
-    expand_limits(x = states_map$long, y = states_map$lat) +
-    scale_fill_continuous(type = "viridis", na.value = "white") + # making NA values = white and scaling using viridis palette
-    labs(title = "Locations of U.S. Ph.D. Granting Institutions for University of Michigan Postdocs",
-         fill = "Percent of Respondents") +
-    coord_map() + # gives map proper dimensions
-    #fifty_states_inset_boxes() + # package function to add boxes around insets for AK and HI
-    theme(axis.title = element_blank(),
-          axis.text = element_blank(),
-          axis.ticks = element_blank(),
-          panel.grid = element_blank(),
-          panel.background = element_rect(fill = "white"))
-  return(plot)
-}
-
-# plotting the data
-#us_degree_regions <- 
-plot_us_degree_regions(us_region_counts)
-
-
-ggsave(filename = "results/us_degree_locations.png", plot = us_degree_regions, width = 20, dpi = 300)
-
 
 # # playing around with more customized maps
 # library(units)
@@ -145,91 +280,3 @@ ggsave(filename = "results/us_degree_locations.png", plot = us_degree_regions, w
 # us.map <- tigris::states(cb = T, year = 2015)
 # test <- us_location_counts
 # test <- fortify(us.map, region = "GEOID")
-
-# Things to work on:
-# Include map of world around it
-# Include us territories
-# Change percent of respondents to be based on total dataset
-
-
-########## END ##########
-
-
-
-########## generating map of locations outside the US ##########
-
-# list of countries available for mapping
-countries <- map_data("world") %>% # making list of countries with available mapping information
-  pull(region) %>% # pulls out list of countries
-  unique(.) # makes list of unique countries
-
-# creating function to extract country names from responses
-extract_int_region <- function(df) {
-  countries <- map_data("world") %>% # making list of countries with available mapping information
-    pull(region) %>% # pulls out list of countries
-    unique(.) # makes list of unique countries
-  data <- df %>%
-    filter(question_no == "Q11" & !is.na(response)) %>% # filters down to country info
-    mutate(response = str_replace_all(response, "_", " "), # replaces all "_" with spaces to allow text parsing
-           region = tolower(case_when(str_detect(response, regex(paste(paste0("\\b", countries, "\\b"), collapse = "|"), ignore_case = T)) ~ # detecting anything in any case that matches country names with word boundaries on both ends
-                                          str_extract(response, regex(paste(paste0("\\b", countries, "\\b"), collapse = "|"), ignore_case = T)),  # extracting the country information to a new col
-                                        TRUE ~ NA_character_))) # any country not in the list of available countries is labelled with NA
-    return(data)
-}
-
-# creating df of locations
-int_data <- extract_int_region(tidy_survey_data)
-
-library(ggalt) # coord_proj
-
-# calculating freqs of countries
-int_region_counts <- get_region_counts(int_data) %>% 
-  select(region, percent_freq) %>% # selects only the region and count cols 
-  right_join(select(as_data_frame(tolower(countries)), value), by = c("region" = "value")) # joining with list of countries to add missing ones
-
-# world_map <- map_data("world") %>% 
-#   mutate(region = tolower(region)) 
-# 
-# int_region_counts %>% 
-#   ggplot(aes(map_id = region)) +
-#   geom_map(map=world_map, aes(fill=percent_freq), colour="black", size=0.5) +
-#   expand_limits(x = world_map$long, y = world_map$lat) +
-#   scale_fill_continuous(type = "viridis", na.value = "white") +
-#   coord_proj("+proj=wintri")
-
-plot_int_degree_regions <- function(df) {
-  world_map <- map_data("world") %>% 
-    mutate(region = tolower(region))
-  
-  plot <- df %>% 
-    ggplot(aes(map_id = region)) +
-    geom_map(aes(fill = percent_freq), map = world_map, colour = "black", size = 0.5) + # plots the map
-    expand_limits(x = world_map$long, y = world_map$lat) +
-    scale_fill_continuous(type = "viridis", na.value = "white") + # making NA values = white and scaling using viridis palette
-    labs(title = "Locations of Ph.D. Granting Institutions for University of Michigan Postdocs",
-         fill = "Percent of Respondents") +
-    coord_proj("+proj=wintri")
-  # #fifty_states_inset_boxes() + # package function to add boxes around insets for AK and HI
-  #   theme(axis.title = element_blank(),
-  #         axis.text = element_blank(),
-  #         axis.ticks = element_blank(),
-  #         panel.grid = element_blank(),
-  #         panel.background = element_rect(fill = "white"))
-  return(plot)
-}
-
-
-plot_int_degree_regions(int_region_counts)
-
-
-
-
-
-
-
-
-########## END ##########
-
-detach("package:mapproj", unload = TRUE)
-detach("package:maps", unload = TRUE)
-
