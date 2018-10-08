3409f3c4ec18f8b025fb480444f9085d0c56e656
William L. Close <close.will@gmail.com>
2018-10-07 23:49:25 -0400

    starting to work on analysis of typed responses

---
+++ code/plot_text_responses.R
@@ -0,0 +1,33 @@
+# sentiment scores (several options)
+  # do by question and include most pos/neg words
+  # look at emotions (fear, joy, etc.)
+  # sentiment of question + sentiment of words in each questions
+# word importance (LDA)
+# word clouds (can do in shape of something)
+# word relationships
+  # networks
+  # bigrams
+  # trigrams
+
+
+# plan:
+  # break responses into individual words (tokenization)
+  # remove common words to focus analysis
+  # pool all responses and analyze each question
+  # break responses up by strat ids and analyze each question
+
+# list of questions with text responses
+  # Q: 
+
+
+# importing stuff ---------------------------------------------------------
+
+# import dataset
+source("code/tidy_survey.R")
+
+
+
+
+# text analysis -----------------------------------------------------------
+
+
