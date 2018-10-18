1a977a07b0147e26e91053136d4c93cdc80419ca
William L. Close <close.will@gmail.com>
2018-10-18 00:12:19 -0400

    Continuing to work on building tf and tf-idf plotting functions

--- code/plot_text_responses.R
+++ code/plot_text_responses.R
@@ -44,7 +44,7 @@
 # importing stuff ---------------------------------------------------------
 
 # import dataset
-source("code/tidy_survey.R")
+source("code/stratify_data.R")
 
 
 library(tidyverse)
@@ -80,36 +80,15 @@ question_list <- unique(tidy_survey_data$question_no)
 # to change the questions included in this list, add/subtract/change the numbers in paste0() as desired
 typed_question_list <- question_list[grepl(paste(paste0("Q", c(23,29,42,43,44,45,46,51)), collapse = "|"), question_list)]
 
-typed_question_df <- tidy_survey_data %>% 
-  filter(question_no %in% typed_question_list) %>% 
-  mutate(response = str_replace_all(response, "_", " ")) %>% 
-  filter(!is.na(response) & !str_detect(response, "\\bn[/]a\\b"))
-
-# creating way to filter out all weird NA responses
-test <- typed_question_df %>% 
-  filter(str_detect(response, regex(paste(paste0("\\b", c("n[/]a", "na"), "\\b"), collapse = "|"), ignore_case = TRUE)))
-
-
-
-#bigrams
-typed_question_df %>% 
-  unnest_tokens(word, response) %>% 
-  #anti_join(stop_words) %>% # removes common words, may be bad for sentiment analysis because removes "no", "not", etc.
-  count(word, sort = TRUE)
-
-
-
-
-
-typed_question_df %>% 
-  mutate(response = str_replace_all(response, "_", " ")) %>% # removing all "_" to prevent identification as a single word
-  unnest_tokens(word, response) %>% # breaks sentences apart into individual rows
-  filter(!word %in% stop_words$word) # filters out stop words which are common words with minimal impact on sentence significance
-
-typed_question_df %>% 
-  filter(question_no == "Q51" & !is.na(response)) %>%
-  createWordCloud()
-
+# typed_question_df <- tidy_survey_data %>% 
+#   filter(question_no %in% typed_question_list) %>% 
+#   mutate(response = str_replace_all(response, "_", " ")) %>% 
+#   filter(!is.na(response) & !str_detect(response, "\\bn[/]a\\b"))
+# 
+# 
+# typed_question_df %>% 
+#   filter(question_no == "Q51" & !is.na(response)) %>%
+#   createWordCloud()
 
 
 
@@ -122,19 +101,6 @@ typed_question_df %>%
 # Term Frequency - Inverse Document Frequency (aka most important words)
 # need to compare questions that are inverse of each other
 # works similar to random forest modelling by finding words that are common but to only a subset of the collection of documents
-
-
-
-# creating test dataset
-example_data <- strat_data$college_school %>% 
-  filter(question_no %in% typed_question_list) %>% # filtering to only contain text based responses
-  filter(question_no == "Q44") %>% # only in place for testing purposes
-  mutate(response = str_replace_all(response, "_", " ")) #%>% # removing extra symbols from tidying
-  # filter(!is.na(response) & !str_detect(response, "\\bn[/]a\\b")) # removing any weird "NA" type responses people input
-
-example_data2 <- tidy_survey_data %>% 
-  filter(question_no %in% typed_question_list) %>% 
-  filter(question_no == "Q44")
 
 
 
@@ -186,50 +152,117 @@ get_n_gram <- function(survey_df, n_token) {
   
 }
 
-# # testing get_n_gram() function to make sure it works
-# get_n_gram(example_data2, 3)
-# get_n_gram(example_data, 1)
-# get_n_gram(example_data, 2)
-# get_n_gram(example_data, 3)
-
 
 
 # creating function to calculate term frequency for entire dataset
-calc_tf <- function(survey_df, n_token) {
+calc_tf <- function(survey_df, n_token, question_no_chr) {
   
   # generating term frequency df
-  tf_df <- get_n_gram(survey_df, n_token) %>% # generating list of n-grams
+  tf_df <- survey_df %>% 
+    filter(question_no == question_no_chr) %>% # filtering based on desired question
+    get_n_gram(n_token) %>% # generating list of n-grams
     count(n_gram, sort = TRUE) %>% # counting occurrences of each n-gram
     mutate(tf = n/sum(n)) %>% # calculating term frequencies based on total number of n-grams in dataset
-    arrange(desc(tf)) # ordering output based on tf value
+    arrange(desc(tf)) %>% # ordering output based on tf value
+    mutate(question = tidy_survey_data %>% filter(question_no == question_no_chr) %>% pull(question) %>% unique()) # adds col with question text back in
   
   # returning the output
   return(tf_df)
   
 }
 
-# # testing calc_tf() function
-# calc_tf(example_data, 1)
-
 
 
 # creating function to calculate tf-idf for stratified data
-calc_tf_idf <- function(survey_df, n_token) {
+calc_tf_idf <- function(survey_df, n_token, question_no_chr) {
   
   # creating df of tf-idf values for each n-gram
-  tf_idf_df <- get_n_gram(survey_df, n_token) %>% # running user function to generate list of n-grams
+  tf_idf_df <- survey_df %>% 
+    filter(question_no == question_no_chr) %>% # filtering based on desired question
+    get_n_gram(n_token) %>% # running user function to generate list of n-grams
     count(strat_id, n_gram, sort = TRUE) %>% # counting instances of each n-gram in each strat_id
     bind_tf_idf(n_gram, strat_id, n) %>% # calculating tf-idf for each n-gram
-    arrange(desc(tf_idf)) # ordering output based on tf-idf value
+    arrange(desc(tf_idf)) %>% # ordering output based on tf-idf value
+    mutate(question = tidy_survey_data %>% filter(question_no == question_no_chr) %>% pull(question) %>% unique()) # adds col with question text back in
   
   # returning output tf-idf values
   return(tf_idf_df)
   
 }
 
-# # testing calc_tf_idf() function
-# calc_tf_idf(example_data, 2)
+calc_tf_idf(example_data, 2, "Q44")
 
+# NOTE: need to change color scheme
+# NOTE: combine calc function and plot function then take out question col from calc function and only have in plot
+
+# creating plotting function for looking at tf for each question/dataset
+plot_tf <- function(tf_df) {
+  
+  tf_data <- tf_df %>% 
+    top_n(20, tf) %>% # pulls out top 20 entries based on tf
+    filter(tf != min(tf)) %>% # removing n-grams that have the smallest tf value for each group (prevents over-plotting)
+    filter(!str_detect(n_gram, "\\bNA\\b")) %>% # filtering out lines containing NA as a token 
+    mutate(n_gram = reorder(n_gram, tf)) # ordering the data to be based on value for nicer looking plots
+  
+  response_no <- length(unique(tf_data$n_gram)) # calculating the number of unique responses for aspect ratio scaling
+  
+  aspect <- 0.2*response_no/7 # scales the aspect ratio to standardize appearance of bars after setting consistent width w/ grobbing
+  
+  tf_plot <- tf_data %>% 
+    ggplot(aes(n_gram, tf)) + # setting the plotting conditions
+    geom_col(show.legend = FALSE) + # graph will be a bar chart without a legend
+    labs(title = str_replace_all(unique(tf_data$question), "_", " "),
+         x = NULL, y = "tf") + # only need the tf value label (n-grams will be other labels)
+    coord_flip(expand = FALSE) + # turns the plot sideways
+    theme(axis.line = element_line(size = 0.5, colour = "black"), # formatting axis lines as desired
+          axis.title = element_text(size = 10), # making all chart titles a consistent size
+          axis.title.x = element_text(margin = margin(10,0,0,0)), # adding space between x axis title and axis labels
+          axis.text = element_text(size = 8),
+          plot.margin = margin(20,20,20,20), # giving plot a bit of padding on edges in case something is plotted out of bounds
+          panel.background = element_rect(fill = "white"), # making panels have white background
+          strip.background.x = element_rect(fill = "white", color = NA), # formatting strip col labels
+          strip.background.y = element_rect(fill = "white", color = NA), # removing border from strip row labels (new y labels)
+          # formatting plots to have a consistent size
+          aspect.ratio = aspect) # making size of bars compared to plot consistent
+  
+  grob_table <- ggplotGrob(tf_plot) # creates a gtable of plot features
+  
+  grob_table$widths[4] <- unit(6, "cm") # sets alignment of y axis in chart area thereby aligning all plots generated with this script (moves chart to the right)
+  grob_table$widths[6] <- unit(6, "cm") # used for alignment (moves chart to the left)
+
+  grobbed_plot <- as_ggplot(arrangeGrob(grob_table)) # recreating the plots with updated coordinates and saving as a ggplot item
+  
+  return(grobbed_plot)
+  
+}
+
+# test5 <- calc_tf(tidy_survey_data, 2, "Q44")
+# pull(test5$question)
+# test5 %>% 
+#   pull(question) %>% 
+#   unique() %>% 
+#   str_replace_all("_", " ")
+# 
+# test5 %>% 
+#   top_n(20, tf) %>% # pulls out top 20 entries based on tf
+#   filter(tf != min(tf)) %>% # removing n-grams that have the smallest tf value for each group (prevents over-plotting)
+#   filter(!str_detect(n_gram, "\\bNA\\b")) %>% # filtering out lines containing NA as a token 
+#   mutate(n_gram = reorder(n_gram, tf)) %>% 
+#   ggplot(aes(n_gram, tf)) + # setting the plotting conditions
+#   geom_col(show.legend = FALSE) + # graph will be a bar chart without a legend
+#   labs(x = NULL, y = "tf", title = str_replace_all(unique(test5$question), "_", " ")) + # only need the tf value label (n-grams will be other labels)
+#   coord_flip(expand = FALSE)
+# 
+# plot_tf(test5)
+# test6$widths
+# 
+# test6$widths[4] <- unit(4, "cm") # sets alignment of y axis in chart area thereby aligning all plots generated with this script
+# test6$widths[6] <- unit(4, "cm")
+# 
+# grobbed_plot <- as_ggplot(arrangeGrob(test6)) # recreating the plots with updated coordinates and saving as a ggplot item
+# grobbed_plot
+
+# map(test2, plot_tf)
 
 
 
@@ -242,136 +275,94 @@ plot_tf_idf <- function(tf_idf_df) {
     group_by(strat_id) %>% # grouping for calculations
     top_n(20, tf_idf) %>% # pulls out top 20 possible entries for each strat_id based on tf-idf
     filter(tf_idf != min(tf_idf)) %>% # removing n-grams that have the smallest tf-idf value for each group (prevents over-plotting)
+    filter(!str_detect(n_gram, "\\bNA\\b")) %>% # filtering out lines containing NA as a token 
     ungroup() %>% # ungroup for plotting
     mutate(n_gram = reorder(n_gram, tf_idf)) %>% # ordering the data to be based on value for nicer looking plots
     ggplot(aes(n_gram, tf_idf, fill = strat_id)) + # setting the plotting conditions
     geom_col(show.legend = FALSE) + # graph will be a bar chart without a legend
     labs(x = NULL, y = "tf-idf") + # only need the tf-idf value label (n-grams will be other labels)
     facet_wrap(~strat_id, ncol = 2, scales = "free") + # making individual plots for each strat_id
-    coord_flip() # turns the plot sideways
+    coord_flip(expand = FALSE) # turns the plot sideways
   
   return(tf_idf_plot)
   
 }
 
 
-test <- calc_tf_idf(example_data, 2)
 
-plot_tf_idf(test)
+### testing things ###
 
-# # looking at unigrams
-# example_unigrams <- example_data %>% 
-#   unnest_tokens(word, response) %>% # breaking responses up into individual words
-#   anti_join(stop_words) %>% # removing any common words
-#   count(strat_id, word, sort = TRUE) %>% # group words by strat_id, count instances of each word, then sort output
-#   ungroup() # ungrouping from count()
+# creating test dataset
+example_data <- strat_data$language %>% 
+  filter(question_no %in% typed_question_list) #%>% # filtering to only contain text based responses
+  #mutate(response = str_replace_all(response, "_", " ")) #%>% # removing extra symbols from tidying
+# filter(!is.na(response) & !str_detect(response, "\\bn[/]a\\b")) # removing any weird "NA" type responses people input
 
-# # calculating tf-idf of unigrams
-# plot_example <- example_unigrams %>% 
-#   bind_tf_idf(word, strat_id, n) %>% # calculating tf-idf of word while grouping on strat_id and using n as input
-#   arrange(desc(tf_idf)) # arranging output
+# example_data2 <- tidy_survey_data %>% 
+#   filter(question_no %in% typed_question_list) %>% 
+#   filter(question_no == "Q44")
 
-# # plotting tf-idf
-# # NOTE: plotting top 15 but a lot of words have the same score so more than 15 are plotted for some strat_id's
-# plot_example %>% 
+# # testing get_n_gram() function to make sure it works
+# get_n_gram(example_data2, 3)
+# get_n_gram(example_data, 1)
+# get_n_gram(example_data, 2)
+# get_n_gram(example_data, 3)
+
+# # testing calc_tf() function
+# calc_tf(example_data, 1)
+
+# # testing calc_tf_idf() function
+# calc_tf_idf(example_data, 2)
+
+# # testing plotting
+# test_question <- "Q23"
+# strat_data$language %>%
+#   filter(question_no == test_question)
+# test <- calc_tf_idf(example_data, 2, test_question)
+# test %>%
 #   group_by(strat_id) %>% # grouping for calculations
-#   top_n(15, tf_idf) %>% # pulls out top 15 entries for each strat_id based on tf-idf
-#   ungroup() %>% 
-#   mutate(word = reorder(word, tf_idf)) %>% # ordering the data for plotting purposes
-#   ggplot(aes(word, tf_idf, fill = strat_id)) + # setting the plotting conditions
-#   geom_col(show.legend = FALSE) +
-#   labs(x = NULL, y = "tf-idf") +
-#   facet_wrap(~strat_id, ncol = 2, scales = "free") + # making individual plots for each strat_id
-#   coord_flip() # turns the plot sideways
+#   top_n(20, tf_idf) %>% # pulls out top 20 possible entries for each strat_id based on tf-idf
+#   filter(tf_idf != min(tf_idf)) %>% # removing n-grams that have the smallest tf-idf value for each group (prevents over-plotting)
+#   filter(!str_detect(n_gram, "\\bNA\\b")) %>% # filtering out lines containing NA as a token
+#   ungroup() %>% # ungroup for plotting
+#   mutate(n_gram = reorder(n_gram, tf_idf))
+# plot_tf_idf(test)
 
 
-# # testing on bigrams
-# # NOTE: need to find way to break ties for tf-idf (ideas: n, tf-idf of unigrams that form bigram)
-# example_data %>% 
-#   unnest_tokens(bigram, response, token = "ngrams", n = 2) %>% # breaking data up into bigrams
-#   count(bigram, sort = TRUE) # looking at rough numbers
-# 
-# example_data %>% 
-#   unnest_tokens(bigram, response, token = "ngrams", n = 2) %>% # breaking data up into bigrams
-#   separate(bigram, c("word1", "word2"), sep = " ") %>% # separating bigrams into individual cols
-#   filter(!word1 %in% stop_words$word) %>% # removing stop words from col1
-#   filter(!word2 %in% stop_words$word) %>% # removing stop words from col2
-#   count(word1, word2, sort = TRUE) # looking at new counts
 
-# # taking out words associated with "postdoc" to focus responses
-# # NOTE: potentially removes some important pieces of information
-# my_stop_words <- add_row(stop_words, word = c("post", "doc", "postdoc", "postdocs", "pd", "pds"),
-#                          lexicon = "custom")
+### running it ###
 
-# # making list of bigrams
-# example_bigrams <- example_data %>% 
-#   unnest_tokens(bigram, response, token = "ngrams", n = 2) %>% # breaking data up into bigrams
-#   separate(bigram, c("word1", "word2"), sep = " ") %>% # separating bigrams into individual cols
-#   filter(!word1 %in% stop_words$word) %>% # removing stop words from col1
-#   filter(!word2 %in% stop_words$word) %>% # removing stop words from col2
-#   unite(bigram, word1, word2, sep = " ") # reuniting words to reform bigrams
-# 
-# # calc tf-idf for each bigram
-# bigram_tf_idf <- example_bigrams %>%
-#   count(strat_id, bigram, sort = TRUE) %>% # counting individual bigrams
-#   bind_tf_idf(bigram, strat_id, n) %>% # calc tf-idf
-#   arrange(desc(tf_idf)) # ordering output based on tf-idf value
+# calc_tf <- function(survey_df, n_token, question_no_chr) 
+# setting up mapping function to loop through all plots and question numbers of stratified data
+# save_all_strat_plots <- function(plot_list, question_no_chr_list, category) {
+#   arguments <- data_frame(plot_name = plot_list,
+#                           question_no_chr = question_no_chr_list)
+#   pmap(arguments, save_strat_plots, category = category)
+# }
 
-# # plotting the data
-# bigram_tf_idf %>% 
-#   group_by(strat_id) %>% # grouping for calculations
-#   top_n(15, tf_idf) %>% # pulls out top 15 entries for each strat_id based on tf-idf
-#   ungroup() %>% 
-#   mutate(bigram = reorder(bigram, tf_idf)) %>% # ordering the data for plotting purposes
-#   ggplot(aes(bigram, tf_idf, fill = strat_id)) + # setting the plotting conditions
-#   geom_col(show.legend = FALSE) +
-#   labs(x = NULL, y = "tf-idf") +
-#   facet_wrap(~strat_id, ncol = 2, scales = "free") + # making individual plots for each strat_id
-#   coord_flip() # turns the plot sideways
+test_function <- function(survey_df, n_token, question_no_chr_list) {
+  arguments <- data_frame(survey_df = list(survey_df),
+                          n_token = n_token,
+                          question_no_chr = question_no_chr_list)
+  data <- pmap(arguments, calc_tf) %>% 
+    set_names(question_no_chr_list)
+  return(data)
+}
+
+test2 <- test_function(example_data, 2, typed_question_list)
+
+map(test2, plot_tf)
 
 
-# # plotting the data v2
-# # pulls out larger number of top responses and removes those with the minimum tf-idf value for each strat_id to prevent overplotting
-# # temporary fix until the data is expanded
-# bigram_tf_idf %>% 
-#   group_by(strat_id) %>% # grouping for calculations
-#   top_n(25, tf_idf) %>% # pulls out top 25 entries for each strat_id based on tf-idf
-#   filter(tf_idf != min(tf_idf)) %>% 
-#   ungroup() %>% 
-#   mutate(bigram = reorder(bigram, tf_idf)) %>% # ordering the data for plotting purposes
-#   ggplot(aes(bigram, tf_idf, fill = strat_id)) + # setting the plotting conditions
-#   geom_col(show.legend = FALSE) +
-#   labs(x = NULL, y = "tf-idf") +
-#   facet_wrap(~strat_id, ncol = 2, scales = "free") + # making individual plots for each strat_id
-#   coord_flip() # turns the plot sideways
 
 
-# # trigrams
-# example_trigrams <- example_data %>% 
-#   unnest_tokens(trigram, response, token = "ngrams", n = 3) %>% # breaking data up into bigrams
-#   separate(trigram, c("word1", "word2", "word3"), sep = " ") %>% # separating bigrams into individual cols
-#   filter(!word1 %in% stop_words$word) %>% # removing stop words from col1
-#   filter(!word2 %in% stop_words$word) %>% # removing stop words from col2
-#   filter(!word3 %in% stop_words$word) %>% 
-#   unite(trigram, word1, word2, word3, sep = " ") # reuniting words to reform bigrams
-# 
-# # calc tf-idf for each trigram
-# trigram_tf_idf <- example_trigrams %>%
-#   count(strat_id, trigram) %>% # counting individual bigrams
-#   bind_tf_idf(trigram, strat_id, n) %>% # calc tf-idf
-#   arrange(desc(tf_idf)) # ordering output based on tf-idf value
 
-# # plotting the data
-# trigram_tf_idf %>% 
-#   group_by(strat_id) %>% # grouping for calculations
-#   top_n(15, tf_idf) %>% # pulls out top 15 entries for each strat_id based on tf-idf
-#   ungroup() %>% 
-#   mutate(trigram = reorder(trigram, tf_idf)) %>% # ordering the data for plotting purposes
-#   ggplot(aes(trigram, tf_idf, fill = strat_id)) + # setting the plotting conditions
-#   geom_col(show.legend = FALSE) +
-#   labs(x = NULL, y = "tf-idf") +
-#   facet_wrap(~strat_id, ncol = 2, scales = "free") + # making individual plots for each strat_id
-#   coord_flip() # turns the plot sideways
+
+
+
+
+
+
 
 # sentiment analysis ------------------------------------------------------
 
