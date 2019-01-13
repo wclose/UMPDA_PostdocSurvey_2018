44a6a3ba3b4440a9e714d64018bf580a380108fd
William Close <close.will@gmail.com>
2019-01-13 02:07:02 -0500

    adding notes on usage

--- code/generate_all_figures.R
+++ code/generate_all_figures.R
@@ -1,5 +1,13 @@
+# This script is used to generate all of the figures for the survey report
+
+# setting survey data to use ----------------------------------------------
+
 # importing survey data
 survey_data <- read_csv("data/raw_data/UMPDA_2018_cross-section_survey.csv")
+
+
+
+# running scripts to generate the various plots ---------------------------
 
 # creating all the plots
 source("code/plot_degree_locations.R")
